425828886f5a9ec8c960da6d1b556744f5fd5efa
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-09 22:10:37 +1000

    ✨ Use 'morgan' middleware for development mode

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
+const morgan = require('morgan');
 
 const app = express();
 
 const restaurantRouter = require('./routes/restaurantRoutes');
+
+// ========== GLOBAL MIDDLEWARE ==========
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // ========== ROUTES ==========
 app.use('/api/restaurants', restaurantRouter);
