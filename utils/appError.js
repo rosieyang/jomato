d42269548facbc1ee606b10867da02a598ba62b1
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-10 16:33:46 +1000

    🥅 Customize error class

---
+++ utils/appError.js
@@ -0,0 +1,15 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = AppError;
