4ef509b6b6173e43bffb6d77d78c723b8d152ac8
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-10 11:10:17 +1000

    ✨ Create initial model schema for Restaurants

---
+++ models/restaurantModel.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    maxlength: [30, 'Name can not be longer than 30 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  website: {
+    type: String,
+  },
+  phone: String,
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  cityCountry: String,
+  cuisine: {
+    type: String,
+    enum: ['Modern Australian', 'Italian', 'Burger', 'Pizza', 'Sandwich', 'Cafe', 'Korean', 'Indian', 'Thai', 'Vietnamese', 'Turkish', 'Chinese', 'Malaysian', 'Japanese', 'Greek', 'Seafood'],
+    required: [true, 'Please add a cuisine']
+  },
+  atmosphere: {
+    type: String,
+    enum: ['casual', 'cosy']
+  },
+  imageCover: String,
+  images: [String],
+  ratingsAverage: {
+    type: Number,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+    default: 0
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  delivery: {
+    type: Boolean,
+    default: false
+  },
+  takeaway: {
+    type: Boolean,
+    default: false
+  },
+  cashOnly: {
+    type: Boolean,
+    default: false
+  },
+  wheelchairAccessible: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Restaurant', restaurantSchema);
