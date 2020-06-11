70159e88239a956fb02630f8bfcc27bd63c088aa
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-11 12:21:27 +1000

    🎨 Apply asyncHandler instead of try/catch blocks

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,10 +1,11 @@
 const AppError = require('../utils/appError');
+const asyncHandler = require('../middleware/asyncHandler');
 const Restaurant = require('../models/restaurantModel');
 
 // @desc        Get all restaurants
 // @route       GET /api/restaurants
 // @access      Public
-exports.getAllRestaurants = async (req, res, next) => {
+exports.getAllRestaurants = asyncHandler(async (req, res, next) => {
   const restaurants = await Restaurant.find();
 
   res.status(200).json({
@@ -12,12 +13,12 @@ exports.getAllRestaurants = async (req, res, next) => {
     count: restaurants.length,
     data: restaurants
   });
-}
+});
 
 // @desc        Get single restaurant
 // @route       GET /api/restaurants/:id
 // @access      Public
-exports.getRestaurant = async (req, res, next) => {
+exports.getRestaurant = asyncHandler(async (req, res, next) => {
   const restaurant = await Restaurant.findById(req.params.id);
 
   if (!restaurant) {
@@ -28,24 +29,24 @@ exports.getRestaurant = async (req, res, next) => {
     status: 'success',
     data: restaurant
   });
-}
+});
 
 // @desc        Create new restaurant
 // @route       POST /api/restaurants
 // @access      Private
-exports.createRestaurant = async (req, res, next) => {
+exports.createRestaurant = asyncHandler(async (req, res, next) => {
   const restaurant = await Restaurant.create(req.body);
 
   res.status(201).json({
     status: 'success',
     data: restaurant
   });
-}
+});
 
 // @desc        Update restaurant
 // @route       PATCH /api/restaurants/:id
 // @access      Private
-exports.updateRestaurant = async (req, res, next) => {
+exports.updateRestaurant = asyncHandler(async (req, res, next) => {
   const restaurant = await Restaurant.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
@@ -59,12 +60,12 @@ exports.updateRestaurant = async (req, res, next) => {
     status: 'success',
     data: restaurant
   });
-}
+});
 
 // @desc        Delete restaurant
 // @route       DELETE /api/restaurants/:id
 // @access      Private (only for owner & admin)
-exports.deleteRestaurant = async (req, res, next) => {
+exports.deleteRestaurant = asyncHandler(async (req, res, next) => {
   const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
 
   if (!restaurant) {
@@ -75,4 +76,4 @@ exports.deleteRestaurant = async (req, res, next) => {
     status: 'success',
     data: restaurant
   });
-}
+});
