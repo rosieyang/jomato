390903fdfd6d3b92fefabb308a1575b1e5e070f1
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-15 18:50:52 +1000

    ✨ Hash user's password using bcryptjs

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,6 +40,12 @@ const userSchema = new mongoose.Schema({
     default: true,
     select: false
   }
+});
+
+// Hash a password before save
+userSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', userSchema);
