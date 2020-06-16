6b405badf240b877d725d6f2147cc7b816b6a1b2
Junghwa Yang <rosie.junghwa@gmail.com>
2020-06-16 15:44:16 +1000

    ✨ Add controller to allow users to get/update/delete user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,3 +71,51 @@ exports.deleteUser = asyncHandler(async (req, res, next) => {
     data: user
   });
 });
+
+// @desc        Get logged in user
+// @route       GET /api/users/me
+// @access      Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  res.status(200).json({
+    status: 'success',
+    data: user
+  });
+});
+
+// @desc        Update logged in user's name or email
+// @route       PATCH /api/users/me
+// @access      Private
+exports.updateMe = asyncHandler(async (req, res, next) => {
+  if (req.body.password) {
+    return next(new AppError("This route is not for updating password. Please use route '/update-password' instead.", 400));
+  }
+
+  const allowedBody = {
+    name: req.body.name || req.user.name,
+    email: req.body.email || req.user.email
+  }
+
+  const user = await User.findByIdAndUpdate(req.user._id, allowedBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: user
+  });
+});
+
+// @desc        Deactivate logged in user
+// @route       DELETE /api/users/me
+// @access      Private
+exports.deleteMe = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user._id, { active: false });
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Your account has been deleted successfully!'
+  });
+});
